2911f25b220a787ef320efa456ff7bb195b744c7
joska <jpotin@gmail.com>
2024-12-09 22:10:15 +0100

    addd ocal storage for palette

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,9 +1,18 @@
 type Palette = [string, string, string, string, string] | []
 
 const getRandomPalette = async () => {
+  // check for local storage
+  if (localStorage.getItem("palettes")) {
+    const palettes = JSON.parse(localStorage.getItem("palettes") || "[]") as Palette[]
+    return palettes[Math.floor(Math.random() * palettes.length)]
+  }
+
+  // if not, fetch and store
   const palettes = (await fetch("https://unpkg.com/nice-color-palettes@3.0.0/100.json").then(
     (response) => response.json()
   )) as Palette[]
+
+  localStorage.setItem("palettes", JSON.stringify(palettes))
   return palettes[Math.floor(Math.random() * palettes.length)]
 }
 
