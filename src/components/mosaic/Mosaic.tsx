3b763eb8bff7d0728e601085194fb6cd845391d1
joska <jpotin@gmail.com>
2024-12-11 18:08:22 +0100

    var(--color-4)

--- src/components/mosaic/Mosaic.tsx
+++ src/components/mosaic/Mosaic.tsx
@@ -20,7 +20,7 @@ const Mosaic = ({ tileWidth = 100, tileHeight = 100, tileSet }: MosaicProps) => 
     ...cssColors,
     "--tile-width": `${tileSize.width}px`,
     "--tile-height": `${tileSize.height}px`,
-    backgroundColor: "var(--color-5)",
+    backgroundColor: "var(--color-4)",
     transition: "background-color 0.5s linear",
   }
 
