667b9aa072230b56633c7489bea4082199a344a2
joska <jpotin@gmail.com>
2024-12-15 00:34:14 +0100

    now maybe cookie fixed

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -77,7 +77,7 @@ const SidebarProvider = React.forwardRef<
         }
 
         // This sets the cookie to keep the sidebar state.
-        document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}; SameSite=None`
+        document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}; SameSite=None; Secure`
       },
       [setOpenProp, open]
     )
