51ea95dbad0a87ff58647cd9d2219ed4c8c5bbd1
joska <jpotin@gmail.com>
2024-12-31 12:24:44 +0100

    ad overflow to the palette picker

--- src/components/mosaic-builder/controls/Palette-picker.tsx
+++ src/components/mosaic-builder/controls/Palette-picker.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const PalettePicker = ({ palettes, currentPalette, handleSetNewColors }: Props) => {
   return (
-    <fieldset className="flex flex-wrap justify-center gap-2 md:gap-4">
+    <fieldset className="flex max-h-[50%] flex-wrap justify-center gap-2 overflow-y-auto py-2 md:gap-4">
       <Palette
         palette={defaultPalette}
         handleSetNewColors={handleSetNewColors}
