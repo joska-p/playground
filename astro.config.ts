fabac23d985ccb925bd470f5d852fccd2af47746
joska <jpotin@gmail.com>
2025-01-03 14:43:19 +0100

    addd sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,14 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), sitemap()],
   site: "https://joska-p.github.io",
   base: "playground",
   trailingSlash: "always",
