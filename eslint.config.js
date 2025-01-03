bedf050258132866f125d945c2b6ff7c0020a6b1
joska <jpotin@gmail.com>
2025-01-03 19:30:43 +0100

    remove bunch of eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,10 @@
 /** @type {import('eslint').Linter.Config[]} */
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { fixupPluginRules, includeIgnoreFile } from "@eslint/compat";
+import { includeIgnoreFile } from "@eslint/compat";
 import pluginJs from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
-import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginAstro from "eslint-plugin-astro";
 import importPlugin from "eslint-plugin-import";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import pluginReact from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -19,57 +13,17 @@ const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  includeIgnoreFile(gitignorePath),
   pluginJs.configs.recommended,
+  includeIgnoreFile(gitignorePath),
   importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs["typescript"],
   ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx,astro}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
     settings: {
       "import/resolver": {
         typescript: true,
       },
     },
-    rules: {
-      "import/no-dynamic-require": "warn",
-      "import/no-named-as-default": "warn",
-    },
   },
-  {
-    ...pluginReact.configs.flat.recommended, // This is not a plugin object, but a shareable config object
-    ...pluginReact.configs.flat["jsx-runtime"], // Add this if you are using React 17+
-    ...jsxA11y.flatConfigs.recommended,
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    plugins: {
-      "react-hooks": fixupPluginRules(reactHooksPlugin),
-    },
-    languageOptions: {
-      globals: globals.browser,
-      ...jsxA11y.flatConfigs.recommended.languageOptions,
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      ...reactHooksPlugin.configs.recommended.rules,
-    },
-  },
-  ...eslintPluginAstro.configs.recommended,
-  eslintConfigPrettier,
 ];
--- src/components/mosaic/mosaic-maker/tiles/cube.tsx
+++ src/components/mosaic/mosaic-maker/tiles/cube.tsx
@@ -8,6 +8,7 @@ type Props = {
 const Cube = ({ colors, rotation, className }: Props) => {
   const styleObjectCube = {
     transform: `rotate(var(${rotation}))`,
+    backgroundColor: `var(${colors[0]})`,
   };
   const styleObjectEdges = {
     borderLeftColor: `var(${colors[1]})`,
@@ -16,20 +17,13 @@ const Cube = ({ colors, rotation, className }: Props) => {
     borderBottomColor: `var(${colors[2]})`,
   };
 
-  const styleObjectinnerSquare = {
-    backgroundColor: `var(${colors[0]})`,
-  };
-
   return (
     <div style={styleObjectCube} className={cn("transition-transform duration-300", className)}>
       <div
         style={styleObjectEdges}
-        className="absolute left-0 top-0 border-b-[calc(var(--tile-width)/2)] border-l-[calc(var(--tile-width)/2)] border-r-[calc(var(--tile-width)/2)] border-t-[calc(var(--tile-width)/2)] border-solid transition-colors duration-300"
+        className="absolute left-0 top-0 border-b-[calc(var(--tile-width)/2)] border-l-[calc(var(--tile-width)/2)] border-r-[calc(var(--tile-width)/2)] border-t-[calc(var(--tile-width)/2)] border-solid transition-[border-color] duration-300"
       />
-      <div
-        style={styleObjectinnerSquare}
-        className="absolute bottom-0 left-0 h-[calc(var(--tile-width)/2)] w-[calc(var(--tile-width)/2)] transition-colors duration-300"
-      ></div>
+      <div className="absolute bottom-0 left-0 h-[calc(var(--tile-width)/2)] w-[calc(var(--tile-width)/2)] bg-inherit transition-[background-color] duration-300"></div>
     </div>
   );
 };
