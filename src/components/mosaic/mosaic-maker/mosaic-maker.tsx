75db6c34d4513aca86a9a471d983b39943c7e4cc
joska <jpotin@gmail.com>
2025-01-26 21:10:56 +0100

    duplicate css fixed. i think

--- src/components/mosaic/mosaic-maker/mosaic-maker.tsx
+++ src/components/mosaic/mosaic-maker/mosaic-maker.tsx
@@ -1,6 +1,5 @@
 import { MosaicMaker } from "@jpotin/mosaic-maker";
 import { StrictMode } from "react";
-import "@jpotin/mosaic-maker/dist/mosaic-maker.css";
 
 function StrictModeMosaicMaker() {
   return (
