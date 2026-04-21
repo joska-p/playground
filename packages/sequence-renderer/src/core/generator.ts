6898b3dd447585186dc9fa323f1a57cfc34ec5bf
joska <jpotin@gmail.com>
2026-04-21 23:02:49 +0200

    add steps limiter logic gard

--- packages/sequence-renderer/src/core/generator.ts
+++ packages/sequence-renderer/src/core/generator.ts
@@ -7,8 +7,9 @@ export const generateSequence = (
   const sequence: number[] = [0];
   const seen = new Set([0]);
   let current = 0;
+  const safeSteps = Math.min(steps, rule.maxSteps);
 
-  for (let i = 1; i < steps; i++) {
+  for (let i = 1; i < safeSteps; i++) {
     current = rule.getNext({
       index: i,
       current,
