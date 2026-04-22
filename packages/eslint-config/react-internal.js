bdecb36109e23a8e218687f09a947b77ce6bd09d
joska <jpotin@gmail.com>
2026-04-22 20:25:47 +0200

    style: enforce function declaration for React components
    Add react/function-component-definition rule to eslint-config

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -32,8 +32,8 @@ export const config = [
     settings: { react: { version: "detect" } },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
+      "react/function-component-definition": ["error", { namedComponents: "function-declaration" }],
     },
   },
 ];
