743de964e922b8957b1e8ae0708bf521e433fa11
joska <jpotin@gmail.com>
2026-04-25 15:48:02 +0200

    add vercel deployement

--- apps/playground/astro.config.mjs
+++ apps/playground/astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
+import process from "node:process";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+// 1. Detect the environment
+const isVercel = process.env.VERCEL === "true";
+
+// 2. Set dynamic values based on environment
+// Default to GH Pages if we aren't on Vercel
+const siteUrl = isVercel ? "https://playground-ten-sand.vercel.app" : "https://joska-p.github.io";
+
+const basePath = isVercel ? "/" : "/playground";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joska-p.github.io",
-  base: "/playground",
+  site: siteUrl, // Used here!
+  base: basePath, // Used here!
   trailingSlash: "ignore",
   integrations: [
     react({
