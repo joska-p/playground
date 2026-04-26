219da9712cb8ff32488268dfd81164265d44f206
joska <jpotin@gmail.com>
2026-04-26 12:46:55 +0200

    extract the docs sorting

--- apps/playground/src/utils/docs.ts
+++ apps/playground/src/utils/docs.ts
@@ -1,5 +1,6 @@
 import { getCollection } from "astro:content";
 
 export async function getDocs() {
-  return getCollection("docs");
+  const docs = await getCollection("docs", ({ data }) => !data.draft);
+  return docs.sort((a, b) => a.data.order - b.data.order);
 }
