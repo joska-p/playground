f11b06e6deb02f0c7414f64e0e99c9ac86146626
joska <jpotin@gmail.com>
2026-04-26 11:31:46 +0200

    Update eslint.config.mjs

--- apps/playground/eslint.config.mjs
+++ apps/playground/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal.js";
+import { config } from "@repo/eslint-config/react-internal";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [...config, ...eslintPluginAstro.configs.recommended];
