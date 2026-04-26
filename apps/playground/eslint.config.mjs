7daff10c2999c490eb4953273b7a219b6848d322
joska <jpotin@gmail.com>
2026-04-26 16:59:56 +0200

    Update eslint.config.mjs

--- apps/playground/eslint.config.mjs
+++ apps/playground/eslint.config.mjs
@@ -1,5 +1,5 @@
 import { globalIgnores } from "eslint/config";
-import { config } from "@repo/eslint-config/base.js";
+import { config } from "@repo/eslint-config/base";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
