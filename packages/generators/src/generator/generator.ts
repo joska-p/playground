56f27ad71314838cfe7c078a04ef7c5c0278e89b
joska <jpotin@gmail.com>
2026-05-05 08:59:56 +0000

    stage generators package

--- packages/generators/src/generator/generator.ts
+++ packages/generators/src/generator/generator.ts
@@ -1,22 +1,5 @@
-import type { Rule } from "../rules/rules.js";
+import { Rule } from "../rules/rules.js";
 
-function generator(rule: Rule, steps: number): number[] {
-  const sequence: number[] = [0];
-  const seen = new Set([0]);
-  let current = 0;
-  const safeSteps = Math.min(steps, rule.maxSteps);
+function* signalGenerator(rule: Rule): Generator<number> {}
 
-  for (let i = 1; i < safeSteps; i++) {
-    current = rule.getNext({
-      index: i,
-      current,
-      sequence,
-      seen,
-    });
-    sequence.push(current);
-    seen.add(current);
-  }
-  return sequence;
-}
-
-export { generator };
+export { signalGenerator };
--- packages/generators/src/index.ts
+++ packages/generators/src/index.ts
@@ -1,2 +1,3 @@
 export { signalGenerator } from "./generator/generator.js";
 export { rules } from "./rules/rules.js";
+export type { Rule } from "./rules/rules.js";
