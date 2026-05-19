6581f71f3b7c4db5f059e54d03d8625fc86d41b4
joska <jpotin@gmail.com>
2026-05-19 13:12:59 +0000

    Refactor color scaling logic

---
+++ packages/palette-generator/src/utils/maths.ts
@@ -0,0 +1,17 @@
+function createRemap(inMin: number, inMax: number) {
+  if (inMin === inMax) {
+    throw new Error("Input range cannot be zero (inMin === inMax)");
+  }
+  return {
+    to(outMin: number, outMax: number) {
+      return {
+        asFloat: (value: number): number =>
+          outMin + ((value - inMin) / (inMax - inMin)) * (outMax - outMin),
+        asInt: (value: number): number =>
+          Math.round(outMin + ((value - inMin) / (inMax - inMin)) * (outMax - outMin)),
+      };
+    },
+  };
+}
+
+export { createRemap };
