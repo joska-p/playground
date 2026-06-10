e7da921ce096a1c289acd317d606cdbc7a82990a
joska <jpotin@gmail.com>
2026-06-10 15:41:36 +0000

    Update PaletteControls.tsx

--- packages/mosaic-maker/src/components/controls/PaletteControls.tsx
+++ packages/mosaic-maker/src/components/controls/PaletteControls.tsx
@@ -12,7 +12,7 @@ function PaletteControls() {
   return (
     <>
       <h3 className="text-foreground/60 sr-only text-xs">Palettes</h3>
-      <div className="flex h-16 flex-wrap justify-center gap-2 overflow-x-auto p-2 md:h-auto">
+      <div className="grid grid-flow-col grid-rows-2 gap-2 overflow-x-auto p-2 h-16 md:flex md:flex-wrap md:h-auto md:justify-center">
         {currentPalettes.map((palette) => (
           <ColorPalette
             key={palette.id}
