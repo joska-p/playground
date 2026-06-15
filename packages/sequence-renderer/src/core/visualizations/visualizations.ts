dc7655eefc029e1bde3bfc4618ba27b412dfc6a1
joska <jpotin@gmail.com>
2026-06-15 12:33:48 +0000

    refactor(sequence-renderer): remove old files

--- packages/sequence-renderer/src/core/visualizations/visualizations.ts
+++
@@ -1,9 +0,0 @@
-import { recamanArcs } from './recamanArcs';
-
-export type Visualization = {
-  id: string;
-  name: string;
-  draw: (options: { canvas: HTMLCanvasElement; sequence: number[] }) => void;
-};
-
-export const visualizations: Visualization[] = [recamanArcs];
