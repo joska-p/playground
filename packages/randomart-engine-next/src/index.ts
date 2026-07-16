7fbb70260d80012dd3d53d88e6cb8e50bf672c49
joska <jpotin@gmail.com>
2026-07-16 15:42:02 +0000

    Remove glsl.ts and consolidate exports in randomart-engine-next
    Directly export GLSL utilities from glsl-library.js to reduce redundant
    file indirection.

--- packages/randomart-engine-next/src/index.ts
+++ packages/randomart-engine-next/src/index.ts
@@ -10,13 +10,12 @@ export { animationRegistry } from './animation.js';
 export { compileToGLSL } from './compileToGLSL.js';
 export { buildTree, evaluate, grow, toGLSL, toStructuredView } from './expression.js';
 export { toMathString, toTreeView } from './format.js';
-//export { generate } from './generate.js';
-export { functionById, glslFunctions, resolveGlslDeps } from './glsl.js';
+export { functionById, glslFunctions, resolveGlslDeps } from './glsl-library.js';
 export { createCorrelatedRng, createDualRng } from './prng.js';
 export { DEFAULT_RULE_ID, getRule, hasRule, listRules } from './rules.js';
 export { WEIGHT_PRESETS, getPresetWeights } from './weight-presets.js';
 
-export type { GlslFunction, GlslFunctionsIds } from './glsl.js';
+export type { GlslFunction, GlslFunctionsIds } from './glsl-library.js';
 export type { DualRng } from './prng.js';
 export type {
   AnimationBehavior,
