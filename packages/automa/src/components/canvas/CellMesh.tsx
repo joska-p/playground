36eb185d30a8d2663e2f86ec918a3191a03f424c
joska <jpotin@gmail.com>
2026-07-30 15:02:06 +0000

    Fix creature lookup in CellMesh component

--- packages/automa/src/components/canvas/CellMesh.tsx
+++ packages/automa/src/components/canvas/CellMesh.tsx
@@ -24,7 +24,7 @@ function CellMesh() {
   const rows = useRows();
   const brushMode = useBrushMode();
   const paletteBrushId = usePaletteBrush();
-  const creature = paletteBrushId;
+  const creature = creatures[paletteBrushId];
   const engineCreated = useRef(false);
   const { canvasRef, runnerRef } = useShaderRunner(fragmentShader);
   const interactionState = useInteractiveCanvas(canvasRef);
