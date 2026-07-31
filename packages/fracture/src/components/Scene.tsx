a1b02ddacf4dc764190840ec9c63f14885b47f4a
joska <jpotin@gmail.com>
2026-07-31 17:50:40 +0000

    Move shader to external file and implement Mandelbrot set

--- packages/fracture/src/components/Scene.tsx
+++ packages/fracture/src/components/Scene.tsx
@@ -1,25 +1,12 @@
 import { ShaderCanvas } from '@repo/graphics/react/ShaderCanvas';
-
-const fragmentShader = /* glsl */ `
-#version 300 es
-precision highp float;
-uniform float u_time;
-
-in vec2 vUv;
-
-out vec4 fragColor;
-
-  void main() {
-    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
-  }
-`.trim();
+import fragmentShader from '../core/mandelbrot.glsl?raw';
 
 function Scene() {
   return (
     <ShaderCanvas
       fragmentShader={fragmentShader}
-      onBeforeRender={(pipeline, time) => {
-        pipeline.setUniforms({ u_time: time });
+      onBeforeRender={(pipeline) => {
+        pipeline.setUniforms({});
       }}
       webGLContextAttributes={{ antialias: true }}
     />
---
+++ packages/fracture/src/core/mandelbrot.glsl
@@ -0,0 +1,24 @@
+#version 300 es
+precision highp float;
+
+in vec2 vUv;
+out vec4 fragColor;
+
+void main() {
+    vec2 c = (vUv - 0.5) * 3.0 - vec2(0.5, 0.0);
+    vec2 z = vec2(0.0);
+    bool diverged = false;
+
+    for (int i = 0; i < 100; i++) {
+        vec2 zSquared = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y);
+        z = zSquared + c;
+
+        if (dot(z, z) > 4.0) {
+            diverged = true;
+            break;
+        }
+    }
+
+    float isInside = diverged ? 0.0 : 1.0;
+    fragColor = vec4(vec3(isInside), 1.0);
+}
