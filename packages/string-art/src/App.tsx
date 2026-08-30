7ac698738d514324097f242c63eafa4d04c8f3cb
joska <jpotin@gmail.com>
2026-08-30 12:45:01 +0000

    feat(image-upload): implement image upload handling and canvas drawing utilities

--- packages/string-art/src/App.tsx
+++ packages/string-art/src/App.tsx
@@ -1,22 +1,48 @@
 import { CpuCanvas } from '@repo/glaze/react/CpuCanvas';
 import { FieldRow, Input } from '@repo/tlc/components/forms';
 import { Shell, ShellCanvas, ShellPanels, Panel } from '@repo/tlc/layout';
-import { useImageUpload } from './hooks/useImageUpload';
+import { useState } from 'react';
+import { calculateImageDimensions, drawImageToCanvas } from './core/utils';
 import type { CpuSurface } from '@repo/glaze/cpu/CpuSurface';
 
 function App() {
-    const [imageFile, handleImageUpload] = useImageUpload();
+    const [surface, setSurface] = useState<CpuSurface | null>(null);
 
-    function handleOnMount(surface: CpuSurface) {
-        console.log('file: ', imageFile);
-        console.log('surface: ', surface);
-        // do stuff
+    function handleImageUpload(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0];
+
+        if (!file || !surface) return;
+
+        console.log('file', file);
+        console.log('surface', surface);
+
+        const { canvas, context } = surface;
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            if (!e.target) return;
+
+            const imageFile = e.target.result as string;
+            const image = new Image();
+
+            image.src = imageFile;
+
+            const dimensions = calculateImageDimensions(
+                image.width,
+                image.height,
+                canvas.width,
+                canvas.height
+            );
+
+            drawImageToCanvas(context, image, dimensions);
+        };
+        reader.readAsDataURL(file);
     }
 
     return (
         <Shell>
             <ShellCanvas>
-                <CpuCanvas onMount={handleOnMount} />
+                <CpuCanvas onMount={setSurface} />
             </ShellCanvas>
 
             <ShellPanels>
